ef57ab5a618fe5202521336ac3f39ef2814f7368
Luan Simões <timpsembs.7@gmail.com>
2023-06-28 20:30:59 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#cadastrar estoque
+#botar preço
+#classificar o produto
+#dar baixa no produto
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', titulo='Gerenciador de Estoque')
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
