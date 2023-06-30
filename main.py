281dbbc32b54e6980e2c7e1193add8e8e74fa3bb
Luan Simões <timpsembs.7@gmail.com>
2023-06-30 09:35:29 -0300

    register functionality done

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "OFKAHWOKPHO@h1hy41-yh02y0ihfaokahkoshkohKOHOKHFKOSAJHF"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/crudsi"
+db = SQLAlchemy(app)
+
+class ProductModel(db.Model):
+    __tablename__ = 'product'
+
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String())
+    product_description = db.Column(db.String())
+    product_price = db.Column(db.Integer())
+    product_quantity = db.Column(db.Integer())
+
+    def __init__(self, product_name, product_description, product_price, product_quantity):
+        self.product_name = product_name
+        self.product_description = product_description
+        self.product_price = product_price
+        self.product_quantity = product_quantity
+
 
 @app.route('/')
 def index():
@@ -14,6 +33,10 @@ def cadastrar():
         product_name = request.form["ProductName"]
         product_quantity = request.form["Quantity"]
         product_description = request.form["Description"]
+        product_price = request.form["Price"]
+        new_product = ProductModel(product_name, product_description, product_price, product_quantity)
+        db.session.add(new_product)
+        db.session.commit()
         flash("Produto cadastrado com sucesso!")
         return redirect(url_for("cadastrar"))
     else:
