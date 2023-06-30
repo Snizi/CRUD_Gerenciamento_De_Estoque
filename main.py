7d4699ed217263708d127d8987b8b558889d9b17
Luan Simões <timpsembs.7@gmail.com>
2023-06-29 21:47:51 -0300

    register route and page refactoring

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from flask import Flask, render_template, request, redirect, session, flash, url
 
 
 app = Flask(__name__)
-
+app.secret_key = "OFKAHWOKPHO@h1hy41-yh02y0ihfaokahkoshkohKOHOKHFKOSAJHF"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cadastrar')
+@app.route('/cadastrar', methods = ["GET", "POST"],)
 def cadastrar():
-    return render_template('cadastro.html')
+    if request.method == "POST":
+        product_name = request.form["ProductName"]
+        product_quantity = request.form["Quantity"]
+        product_description = request.form["Description"]
+        flash("Produto cadastrado com sucesso!")
+        return redirect(url_for("cadastrar"))
+    else:
+        return render_template('cadastro.html')
 
+    
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
